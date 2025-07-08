3c1db1aed69b23803c0986cb0546d33e4306bf57
Jaanu-123-jaanu <jjang5@unh.newhaven.edu>
2025-07-07 20:47:03 -0400

    Update streamlit_app.py
    connected changes needed bc to move SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,12 @@ st.write(
   """
 )
 
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx=st.connection("snowflake")
 session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -26,23 +26,24 @@ ingredients_list = st.multiselect(
 
 
 if ingredients_list:
+
+
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """"','"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)
+    
     time_to_start = st.button('Submit Order')
 
     if time_to_start:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
+        
         st.write(my_insert_stmt)
         st.stop()
-        
